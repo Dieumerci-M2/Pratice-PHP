8db5e25b5d1d0c6109268caab92c8aaa9419f699
Dieumerci Muhindo <dieumerci.md09@gmail.com>
2023-01-11 13:54:57 +0100

    feat : add if else condition for good understanding

--- demo.php
+++ demo.php
@@ -23,4 +23,33 @@ switch(strtoupper($mention)){
         echo "nous avons pas de pourcentage lié à cette lettre veuillez saisir la bonne lettre \n";
         break;
     }
+
+// Exemple avec la condition if else
+
+// Comme deuxième exemple nous allons faire une application qui nous retourneras une mention en entrant un pourcentage
+
+$pourcentage =  (int)readline('Entrer un pourcentage : ');
+
+if(0 < $pourcentage && $pourcentage <= 39){
+    echo "Vous avez la mention naff \n";
+}
+elseif(40 <= $pourcentage && $pourcentage <= 49){
+    echo "Vous avez la mention Ajournée \n";
+}
+elseif(50 <= $pourcentage && $pourcentage <= 69){
+    echo "Vous avez la mention Satisfaction \n";
+}
+elseif(70 <= $pourcentage && $pourcentage <= 79){
+    echo "Vous avez la mention Distinction \n";
+}
+elseif(80 <= $pourcentage && $pourcentage <= 89){
+    echo "Vous avez la mention Grande Distinction \n";
+}
+elseif(90 <= $pourcentage && $pourcentage <= 99){
+    echo "Vous avez la mention Très Grande Distinction \n";
+}
+else{
+    echo "Vous pouvez pas avoir un pourcentage moins que 1 et plus que 99 \n";
+}
+
 ?>
